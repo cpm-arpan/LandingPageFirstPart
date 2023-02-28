66b17ee07626058c78a6ed9695ab8554b682039b
cpm-arpan <121436075+cpm-arpan@users.noreply.github.com>
2023-02-28 09:28:39 +0545

    Completed the carousel part and the testimonial section
    Inserted the slick slider in the landing page and also insertedthe testimonials part with sliders which further caused some errors as the image is not being overflowed outside of the section, working on it.

---
+++ script.js
@@ -0,0 +1,70 @@
+
+$('.lp-featured-products').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3,
+        
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+})
+
+
+
+
+// Slider 2nd for testimonial section=============
+
+$('.testimonial-content').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows:true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
