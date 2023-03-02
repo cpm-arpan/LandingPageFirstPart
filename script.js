c67daac97325fcfc1664b6ba46c2cd0de58df5ce
cpm-arpan <121436075+cpm-arpan@users.noreply.github.com>
2023-03-02 13:45:14 +0545

    Accordions finally done.
    Accordions finally started working smoothly

--- script.js
+++ script.js
@@ -72,10 +72,3 @@ $('.testimonial-content').slick({
 
 
 
-const accordion = document.getElementsByClassName('container');
-
-for (i=0; i<accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-  })
-}
